b233d4ae6d1f38006da39949621759f4d0bd075b
pstevenkent <pstevenkent@student.ciputra.ac.id>
2023-10-18 15:47:53 +0700

    textalign

--- app/src/main/java/com/example/reposi/MainActivity.kt
+++ app/src/main/java/com/example/reposi/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("WUWUNG Suka WUWUNG")
+                    Greeting("WUWUNG Suka WUWUNG SUKA WILLAS")
                 }
             }
         }
